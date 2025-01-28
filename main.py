3258df4192a46d2660af215080b4e39e8596581f
kapishash <kapishashtankar10@gmail.com>
2025-01-28 11:49:12 +0530

    base model vgg16

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.disease_classification import logger
 from src.disease_classification import logger
 from src.disease_classification.pipeline.data_ingestion import DataIngestionTrainingPipeline
-# from disease_classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.disease_classification.pipeline.prepare_base_model import PrepareBaseModelTrainingPipeline
 # from disease_classification.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 # from disease_classification.pipeline.stage_04_evaluation import EvaluationPipeline
 
@@ -12,6 +12,18 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
